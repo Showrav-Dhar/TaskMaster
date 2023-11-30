b177a7da4c1ed1ae75518cd2f65308511e3c1f6e
showrav dhar <dhardip349900@gmail.com>
2023-11-30 17:45:49 +0600

    details

--- base/views.py
+++ base/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 # from django.http import HttpResponse #step 1 
 #as we are doing class based view we can remove the HttpResponse instead of that we will use the following line 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
-def TaskList(request): #step 2
-    model = Task
+
+class TaskList(ListView): 
+    model = Task#this class now search for templates that has name like - Task_list.html
+    context_object_name = 'tasks'
 
 
+class TaskDetail(DetailView):
+    model = Task#this class now search for templates that has name like - Task_detail.html
