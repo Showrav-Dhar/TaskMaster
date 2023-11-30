a684176276c822fb289883c59cc81895cc076fda
showrav dhar <dhardip349900@gmail.com>
2023-11-30 17:23:30 +0600

    UPD1

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path #accessing all views from the taskmaster folder
-from . import views #accessing all views from the base folder
+# from . import views #accessing all views from the base folder
+from .views import TaskList
 
 urlpatterns = [
-    path('', views.tasklist, name='tasks'), #this is base url that is empty string in the beginning
+    path('', TaskList.as_view(), name='tasks'), #this is base url that is empty string in the beginning
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse #step 1
 # Create your views here.
+# from django.http import HttpResponse #step 1 
+#as we are doing class based view we can remove the HttpResponse instead of that we will use the following line 
+from django.views.generic.list import ListView
+from .models import Task
 
-def tasklist(request): #step 2
-    return HttpResponse('To Do List')
+def TaskList(request): #step 2
+    model = Task
+
+
