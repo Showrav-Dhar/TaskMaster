0407f14e6e28104185662ecfd172dd4aef7a2b9d
showrav dhar <dhardip349900@gmail.com>
2023-12-24 20:23:54 +0600

    upto update view

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path #accessing all views from the taskmaster folder
 # from . import views #accessing all views from the base folder
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'), #this is base url that is empty string in the beginning
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#task/the task number
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 #as we are doing class based view we can remove the HttpResponse instead of that we will use the following line 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy#redirect user sucessfully to a different page
 from .models import Task
 
 
@@ -16,3 +18,17 @@ class TaskDetail(DetailView):
     model = Task#this class now search for templates that has name like - Task_detail.html
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):#automatically created form and used the data from task model
+    #this view looks for a template which name ends with _form.html
+    #if we used function based view instead of class based view we would have to do all the work manually 
+    model = Task
+    # fields = ['title','description']
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')#when we create a item send the user back to the list
+
+#update view modifies the data
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
