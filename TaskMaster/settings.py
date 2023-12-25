736561041265d672aa471395b6d8b382fac519ec
showrav dhar <dhardip349900@gmail.com>
2023-12-26 03:00:25 +0600

    upd

--- TaskMaster/settings.py
+++ TaskMaster/settings.py
@@ -112,6 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'#sents user to the login page if they are not authenticated
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse #step 1 
@@ -28,27 +31,36 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task#this class now search for templates that has name like - Task_list.html
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count
+        return context
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task#this class now search for templates that has name like - Task_detail.html
     context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):#automatically created form and used the data from task model
+class TaskCreate(LoginRequiredMixin, CreateView):#automatically created form and used the data from task model
     #this view looks for a template which name ends with _form.html
     #if we used function based view instead of class based view we would have to do all the work manually 
     model = Task
-    # fields = ['title','description']
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')#when we create a item send the user back to the list
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+
 #update view modifies the data
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     #this view looks for a template which name ends with _confirm_delete.html
     model = Task
     context_object_name = 'task' #tasks
